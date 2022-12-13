1672d6e63ad16e467093611e01c152a97c17fa99
Vitor Goncalves Lima <vitorgl@ciandt.com>
2022-12-13 13:55:42 -0300

    Unit testing item Controller and Basic JSON Assertations

---
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/controller/ItemControllerTest.java
@@ -0,0 +1,41 @@
+package com.in28minutes.unittesting.unittesting.business.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import com.in28minutes.unittesting.unittesting.controller.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+
+
+
+
+@WebMvcTest(ItemController.class)
+public class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void dummyItem_basic() throws Exception {
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/dummy-item")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}"))
+                .andReturn();
+    }
+
+    }
