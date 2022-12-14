063338b78d52d04f946446d4ac336408a3221900
Vitor Goncalves Lima <vitorgl@ciandt.com>
2022-12-14 15:10:00 -0300

    Writing an Integration Test Using @SpringBootTest

---
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/ItemControllerIT.java
@@ -0,0 +1,24 @@
+package com.in28minutes.unittesting.unittesting.business;
+
+
+import org.json.*;
+import org.junit.jupiter.api.*;
+import org.skyscreamer.jsonassert.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.boot.test.web.client.*;
+
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ItemControllerIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void contextLoads() throws JSONException {
+        String response = this.restTemplate.getForObject("/all-items-from-database", String.class);
+        JSONAssert.assertEquals("[{id:10001}, {id:10002}, {id:10003}]", response, false);
+    }
+
+}
